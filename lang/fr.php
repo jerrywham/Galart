0c53eca82d8755048d7f6cbc7d5c952c94eddb10
jerrywham <github@ecyseo.net>
2017-08-21 15:01:28 +0200

    Create fr.php

---
+++ lang/fr.php
@@ -0,0 +1,8 @@
+<?php 
+	$LANG = array(
+		"L_TITLE"=>"Galart",
+		"L_DESCRIPTION"=>"Lier un dossier d'images à un article pour générer une galerie automatiquement",
+		"L_NO"=>"Non",
+		"L_YES"=>"Oui",
+		"L_ARTICLE_FOLDER"=>"Pour associer un dossier image à l'article, sélectionnez-le ici:",
+	);
