adc4c195c1a59f0ab917877216339867179f01ec
jerrywham <github@ecyseo.net>
2017-08-24 01:57:29 +0200

    Update config.php

--- config.php
+++ config.php
@@ -8,7 +8,9 @@
 **/
 if(!defined("PLX_ROOT")) exit; ?>
 <?php 
+	$plxAdmin = plxAdmin::getInstance();
 	if(!empty($_POST)) {
+		$plxPlugin->setParam("rep", ($_POST["rep"] == '' ? $plxAdmin->aConf["medias"].'images/' : plxUtils::strCheck($_POST["rep"])), "string");
 		$plxPlugin->setParam("size", plxUtils::strCheck($_POST["size"]), "numeric");
 
 		$plxPlugin->saveParams();
@@ -19,9 +21,14 @@ if(!defined("PLX_ROOT")) exit; ?>
 <h2><?php $plxPlugin->lang("L_TITLE") ?></h2>
 <p><?php $plxPlugin->lang("L_DESCRIPTION") ?></p>
 <form action="parametres_plugin.php?p=Galart" method="post" >
-
+		<p>
+		<label>Répertoire principal des images : 
+			<input type="text" name="rep" value="<?php echo ($plxPlugin->getParam("rep") == '' ? $plxAdmin->aConf["medias"].'images/' : $plxPlugin->getParam("rep"));?>"/>
+            <a class="hint"><span><?php echo L_HELP_SLASH_END ?></span></a>&nbsp;<strong>ex: data/medias/images/</strong>
+		</label>
+		</p>
 		<label>taille des miniatures : 
-			<input type="text" style="width:100%;" name="size" value="<?php echo $plxPlugin->getParam("size");?>"/>
+			<input type="text" name="size" value="<?php echo $plxPlugin->getParam("size");?>"/>
 		</label>
 
 
